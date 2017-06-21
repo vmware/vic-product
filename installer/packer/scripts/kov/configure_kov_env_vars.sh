2289ab13cb6cd01fdb21e155b4aba4b476736bc7
maplain <maplainfly@gmail.com>
2017-06-21 01:53:31 -0700

    add kov base.vmdk to vic.ova

--- installer/packer/scripts/kov/configure_kov_env_vars.sh
+++ installer/packer/scripts/kov/configure_kov_env_vars.sh
@@ -4,6 +4,7 @@ data_dir=/data/kov
 cert_dir=${data_dir}/cert
 cert=${cert_dir}/server.crt
 key=${cert_dir}/server.key
+vmdk=${data_dir}/base_$(cat /etc/vmware/kov/kovd_revision).vmdk
 
 port="$(ovfenv -k cluster_manager.port)"
 mkdir -p /etc/vmware/kov
@@ -12,4 +13,6 @@ cat > /etc/vmware/kov/kov_env_vars <<EOF
 KOVD_EXPOSED_PORT=${port}
 KOVD_KEY_LOCATION=$key
 KOVD_CERT_LOCATION=$cert
+KOV_VMDK_LOCATION=$vmdk
+KOVD_DATA_DIR=$data_dir
 EOF
