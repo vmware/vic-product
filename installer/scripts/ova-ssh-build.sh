0e1e069ea8d3932b9f378b72a505ed21b3eabc1d
Ashok Rajan <rashok@vmware.com>
2017-07-19 15:56:38 -0700

    WIP: Setting up the automatic drone OVA build in CI

--- installer/scripts/ova-ssh-build.sh
+++ installer/scripts/ova-ssh-build.sh
@@ -45,4 +45,5 @@ echo "Starting ssh to the OVA Builder vm.."
 ssh -t -o StrictHostKeyChecking=no -i $keyfile $OVA_BUILD_USER@$OVA_BUILD_MACHINE_IP "ping -c 3 $OVA_BUILD_MACHINE_IP && traceroute $OVA_BUILD_MACHINE_IP"
 
 echo "Copying the ova from the ovabuilder.."
-scp -v -r -o StrictHostKeyChecking=no -i $keyfile $OVA_BUILD_USER@$OVA_BUILD_MACHINE_IP:"~/go/src/github.com/vmware/vic-product/installer/bin/vic-*.ova" bin/
+#scp -v -r -o StrictHostKeyChecking=no -i $keyfile $OVA_BUILD_USER@$OVA_BUILD_MACHINE_IP:"~/go/src/github.com/vmware/vic-product/installer/bin/vic-*.ova" bin/
+rsync -r -v --progress -e ssh -o StrictHostKeyChecking=no -i $keyfile $OVA_BUILD_USER@$OVA_BUILD_MACHINE_IP:"~/go/src/github.com/vmware/vic-product/installer/bin/vic-*.ova" bin/
