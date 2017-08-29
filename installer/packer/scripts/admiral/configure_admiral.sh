dfde4c23db6a259083961b941298d3216d627360
Andrew Chin <andrewtchin@users.noreply.github.com>
2017-08-29 14:16:36 -0500

    Increase self signed cert expiration (#685)

--- installer/packer/scripts/admiral/configure_admiral.sh
+++ installer/packer/scripts/admiral/configure_admiral.sh
@@ -64,7 +64,7 @@ function genCert {
   if [ ! -e $ca_cert ] || [ ! -e $ca_key ]
   then
     openssl req -newkey rsa:4096 -nodes -sha256 -keyout $ca_key \
-      -x509 -days 365 -out $ca_cert -subj \
+      -x509 -days 1095 -out $ca_cert -subj \
       "/C=US/ST=California/L=Palo Alto/O=VMware, Inc./OU=Containers on vSphere/CN=Self-signed by VMware, Inc."
   fi
   openssl req -newkey rsa:4096 -nodes -sha256 -keyout $key \
@@ -73,7 +73,7 @@ function genCert {
 
   echo "Add subjectAltName = IP: $ip_address to certificate"
   echo subjectAltName = IP:"$ip_address" > $ext
-  openssl x509 -req -days 365 -in $csr -CA $ca_cert -CAkey $ca_key -CAcreateserial -extfile $ext -out $cert
+  openssl x509 -req -days 1095 -in $csr -CA $ca_cert -CAkey $ca_key -CAcreateserial -extfile $ext -out $cert
 
   echo "Creating certificate chain for $cert"
   cat $ca_cert >> $cert
