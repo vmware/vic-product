fcaa6c00c2ac874baa005d482aed47a4335cd36a
Jason Morris <morris-jason@users.noreply.github.com>
2017-10-06 17:03:00 -0500

    Implement packerless ova build
    Implements a packerless ova build using a
    Linux kernel install and dependency extraction
    inside a docker container. Fixes #708, #669.

---
+++ installer/build/build.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# Copyright 2017 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# exit on failure and configure debug, include util functions
+set -eu -o pipefail +h
+
+DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER:-}
+ADMIRAL=""
+VICENGINE=""
+HARBOR=""
+
+function usage() {
+    echo -e "Usage: 
+      <ova-dev|ova-ci>
+      [--admiral] <given a revision, eg. 'dev'>
+      [--vicengine|--harbor] <given a url, eg. 'https://storage.googleapis.com/vic-engine-builds/vic_13806.tar.gz'>
+      [--vicengine|--harbor] <given a file in cwd, eg. 'vic_13806.tar.gz'>
+    ie: $0 ova-dev --harbor v1.2.0-38-ge79334a --vicengine https://storage.googleapis.com/vic-engine-builds/vic_13806.tar.gz --admiral v1.2" >&2
+    exit 1
+}
+
+# Check if it's a file in `scripts`, URL, or REVISION
+function setenv() {
+  tmpvar=$1
+  fallback=$2
+  # if a cli argument was specified, it's a local file, gcloud bucket upload, or admiral revision number.
+  if [ -n "${!tmpvar}" ]; then
+    if [ -f "$(pwd)/build/${!tmpvar}" ]; then
+      export BUILD_$1_FILE="${!tmpvar}"
+      export BUILD_$1_URL="$(pwd)/build/${!tmpvar}"
+    elif [[ "${!tmpvar}" =~ ^http://|^https:// ]]; then
+      export BUILD_$1_URL="${!tmpvar}"
+      export BUILD_$1_FILE="$(basename "${!tmpvar}")"
+    else
+      export BUILD_$1_REVISION="${!tmpvar}"
+    fi
+  else   # if a cli argument was NOT specified, use the most recent version specified by $fallback
+    if [[ "${fallback}" =~ ^http://|^https:// ]]; then
+      export BUILD_$1_URL="${fallback}"
+      export BUILD_$1_FILE=$(basename "${fallback}")
+    else
+      export BUILD_$1_REVISION="${fallback}"
+    fi
+  fi
+}
+
+export BUILD_OVA_REVISION=$(git describe --tags)
+export BUILD_DCHPHOTON_VERSION="1.13"
+
+step=$1; shift
+[ ! "$step" == "ova-ci" ] || [ ! "$step" == "ova-dev" ] || usage
+
+while [[ $# -gt 1 ]]
+do
+  key="$1"
+
+  case $key in
+    --admiral)
+      ADMIRAL="$2"
+      shift # past argument
+      ;;
+    --vicengine)
+      VICENGINE="$2"
+      shift # past argument
+      ;;
+    --harbor)
+      HARBOR="$2"
+      shift # past argument
+      ;;
+    --step)
+      STEP="$2"
+      shift
+      ;;
+    *)
+      # unknown option
+      usage
+      exit 1
+      ;;
+  esac
+  shift # past argument or value
+done
+
+# set Admiral
+setenv ADMIRAL "dev"
+
+# set Vic-Engine
+url=$(gsutil ls -l "gs://vic-engine-builds" | grep -v TOTAL | grep vic_ | sort -k2 -r | head -n1 | xargs | cut -d ' ' -f 3 | sed 's/gs:\/\//https:\/\/storage.googleapis.com\//')
+setenv VICENGINE "$url"
+
+#set Harbor
+url=$(gsutil ls -l "gs://harbor-builds" | grep -v TOTAL | sort -k2 -r | head -n1 | xargs | cut -d ' ' -f 3 | sed 's/gs:\/\//https:\/\/storage.googleapis.com\//')
+setenv HARBOR "$url"
+
+
+ENV_FILE=build/baseimage/root/installer.env
+touch $ENV_FILE
+cat > $ENV_FILE <<EOF
+export BUILD_HARBOR_FILE=${BUILD_HARBOR_FILE:-}
+export BUILD_HARBOR_URL=${BUILD_HARBOR_URL:-}
+export BUILD_VICENGINE_FILE=${BUILD_VICENGINE_FILE:-}
+export BUILD_VICENGINE_URL=${BUILD_VICENGINE_URL:-}
+export BUILD_ADMIRAL_REVISION=${BUILD_ADMIRAL_REVISION:-}
+export BUILD_OVA_REVISION=${BUILD_OVA_REVISION:-}
+export BUILD_DCHPHOTON_VERSION=${BUILD_DCHPHOTON_VERSION-}
+export DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER:-}
+EOF
+
+echo -e "--------------------------------------------------
+buidling ova with env...\n
+$(cat $ENV_FILE | sed 's/export //g')
+--------------------------------------------------"
+
+echo "--------------------------------------------------"
+echo "caching build dependencies..."
+mkdir -p build/baseimage/{bin,cache,cache/docker}
+make ova
+./build/cache.sh
+
+echo "--------------------------------------------------"
+echo "packaging OVA..."
+
+if [ "$step" == "ova-dev" ]; then
+docker run --rm --privileged -v /dev:/dev -v $(pwd):/work -w /work \
+    -e BUILD_HARBOR_FILE=${BUILD_HARBOR_FILE} \
+    -e BUILD_VICENGINE_FILE=${BUILD_VICENGINE_FILE} \
+    -e BUILD_ADMIRAL_REVISION=${BUILD_ADMIRAL_REVISION} \
+    -e BUILD_OVA_REVISION=${BUILD_OVA_REVISION} \
+    -e BUILD_DCHPHOTON_VERSION=${BUILD_DCHPHOTON_VERSION} \
+    -e DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER} \
+    -e TERM vmware/photon ./build/baseimage/stage.sh
+elif [ "$step" == "ova-ci" ]; then
+  ./build/baseimage/stage.sh
+else
+  usage
+fi
+
+cp build/vic-unified.ovf build/baseimage/bin/vic-${BUILD_OVA_REVISION}.ovf
+cd build/baseimage/bin/ 
+sed -i -e s~--version--~${BUILD_OVA_REVISION}~ vic-${BUILD_OVA_REVISION}.ovf
+echo "rebuilding OVF manifest"
+sha256sum --tag * | sed s/SHA256\ \(/SHA256\(/ > vic-${BUILD_OVA_REVISION}.mf
+tar -cvf ../../../bin/vic-${BUILD_OVA_REVISION}.ova vic-${BUILD_OVA_REVISION}.ovf vic-${BUILD_OVA_REVISION}.mf *.vmdk
+cd ../../../
+echo "--------------------------------------------------"
+echo "cleaning up..."
+./build/cleanup.sh
+
+OUTFILE=bin/$(ls -1t bin | grep "\.ova")
+
+if [ -n "${DRONE_BUILD_NUMBER}" ]; then
+  TMP=$(echo "${OUTFILE}" | sed "s/-/-${DRONE_BUILD_NUMBER}-/")
+  mv "${OUTFILE}" "${TMP}"
+  OUTFILE=${TMP}
+fi
+
+echo "build complete"
+echo "  SHA256: $(shasum -a 256 $OUTFILE)"
+echo "  SHA1: $(shasum -a 1 $OUTFILE)"
+echo "  MD5: $(md5sum $OUTFILE)"
+du -ks $OUTFILE | awk '{printf "%sMB\n", $1/1024}'
---
+++ installer/build/scripts/admiral/configure_admiral.sh
@@ -0,0 +1,224 @@
+#!/usr/bin/bash
+# Copyright 2017 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -euf -o pipefail
+
+umask 077
+
+ADMIRAL_PORT=$(ovfenv -k management_portal.port)
+
+data_dir="/data/admiral"
+conf_dir="/etc/vmware/admiral"
+script_dir="/etc/vmware"
+keytool="/usr/bin/keytool"
+
+cert_dir="${data_dir}/cert"
+flag="${data_dir}/cert_gen_type"
+admiral_start_script="${conf_dir}/start_admiral.sh"
+admiral_add_default_users_script=${conf_dir}/add_default_users.sh
+
+ca_download_dir="${data_dir}/ca_download"
+mkdir -p "${cert_dir}"
+rm -rf "${ca_download_dir}"
+mkdir -p "${ca_download_dir}"
+
+cert="${cert_dir}/server.crt"
+key="${cert_dir}/server.key"
+jks="${cert_dir}/trustedcertificates.jks"
+csr="${cert_dir}/server.csr"
+ca_cert="${cert_dir}/ca.crt"
+ca_key="${cert_dir}/ca.key"
+ext="${cert_dir}/extfile.cnf"
+
+#Configure attr in script
+function configureScript {
+  script_name=$1
+  cfg_key=$2
+  cfg_value=$3
+
+  if [ -n "$cfg_key" ]; then
+    cfg_value=$(echo "$cfg_value" | sed -r -e 's%[\/&%]%\\&%g')
+    sed -i -r "s%#?$cfg_key\s*=\s*.*%$cfg_key=$cfg_value%" $script_name
+  fi
+}
+
+# Format cert file
+function formatCert {
+  content=$1
+  file=$2
+  echo "$content" | sed -r 's/(-{5}BEGIN [A-Z ]+-{5})/&\n/g; s/(-{5}END [A-Z ]+-{5})/\n&\n/g' | sed -r 's/.{64}/&\n/g; /^\s*$/d' > "$file"
+}
+
+function genCert {
+  if [ ! -e $ca_cert ] || [ ! -e $ca_key ]
+  then
+    openssl req -newkey rsa:4096 -nodes -sha256 -keyout $ca_key \
+      -x509 -days 1095 -out $ca_cert -subj \
+      "/C=US/ST=California/L=Palo Alto/O=VMware, Inc./OU=Containers on vSphere/CN=Self-signed by VMware, Inc."
+  fi
+  openssl req -newkey rsa:4096 -nodes -sha256 -keyout $key \
+    -out $csr -subj \
+    "/C=US/ST=California/L=Palo Alto/O=VMware/OU=Containers on vSphere/CN=$hostname"
+
+  echo "Add subjectAltName = IP: $ip_address to certificate"
+  echo subjectAltName = IP:"$ip_address" > $ext
+  openssl x509 -req -days 1095 -in $csr -CA $ca_cert -CAkey $ca_key -CAcreateserial -extfile $ext -out $cert
+
+  echo "Creating certificate chain for $cert"
+  cat $ca_cert >> $cert
+
+  echo "self-signed" > $flag
+  echo "Copy CA certificate to $ca_download_dir"
+  cp $ca_cert $ca_download_dir/
+  $script_dir/set_guestinfo.sh -f $ca_cert "admiral.ca"
+
+  echo "creating java keystore with self-signed CA"
+  if [ -f "$jks" ]; then
+    echo "removing existing keystore $jks"
+    rm -f $jks
+  else
+    echo "no keystore present, creating"
+  fi
+
+  $keytool -import -noprompt -v -trustcacerts -alias selfsignedca -file $ca_cert -keystore $jks -keypass changeit -storepass changeit
+}
+
+function secure {
+  ssl_cert=$(ovfenv -k management_portal.ssl_cert)
+  ssl_cert_key=$(ovfenv -k management_portal.ssl_cert_key)
+  ca_cert_input=$(ovfenv -k management_portal.ca_cert)
+  if [ -n "$ssl_cert" ] && [ -n "$ssl_cert_key" ] && [ -n "$ca_cert_input" ]; then
+    echo "ssl_cert, ssl_cert_key, and ca_cert are set, using customized certificate"
+    formatCert "$ssl_cert" $cert
+    formatCert "$ssl_cert_key" $key
+    formatCert "$ca_cert_input" $ca_cert
+    echo "customized" > $flag
+    echo "creating java keystore with provided cert for xenon"
+    if [ -f "$jks" ]; then
+      echo "removing existing keystore $jks"
+      rm -f $jks
+    else
+      echo "no keystore present, creating"
+    fi
+    $keytool -import -noprompt -v -trustcacerts -alias selfsignedca -file $cert -keystore $jks -keypass changeit -storepass changeit
+    return
+  fi
+
+  if [ ! -e $ca_cert ] || [ ! -e $cert ] || [ ! -e $key ]; then
+    echo "CA, Certificate or key file does not exist, will generate a self-signed certificate"
+    genCert
+    return
+  fi
+
+  if [ ! -e $flag ]; then
+    echo "The file which records the way generating certificate does not exist, will generate a new self-signed certificate"
+    genCert
+    return
+  fi
+
+  if [ ! "$(cat $flag)" = "self-signed" ]; then
+    echo "The way generating certificate changed, will generate a new self-signed certificate"
+    genCert
+    return
+  fi
+
+  cn=$(openssl x509 -noout -subject -in $cert | sed -n '/^subject/s/^.*CN=//p') || true
+  if [ "$hostname" !=  "$cn" ]; then
+    echo "Common name changed: $cn -> $hostname , will generate a new self-signed certificate"
+    genCert
+    return
+  fi
+
+  ip_in_cert=$(openssl x509 -noout -text -in $cert | sed -n '/IP Address:/s/.*IP Address://p') || true
+  if [ "$ip_address" !=  "$ip_in_cert" ]; then
+    echo "IP changed: $ip_in_cert -> $ip_address , will generate a new self-signed certificate"
+    genCert
+    return
+  fi
+
+  echo "Use the existing CA, certificate and key file"
+  echo "Copy CA certificate to $ca_download_dir"
+  cp $ca_cert $ca_download_dir/
+}
+
+function detectHostname {
+  hostname=$(hostnamectl status --static) || true
+  if [ -n "$hostname" ]; then
+    if [ "$hostname" = "localhost.localdomain" ]; then
+      hostname=""
+      return
+    fi
+    echo "Get hostname from command 'hostnamectl status --static': $hostname"
+    return
+  fi
+}
+
+hostname=""
+ip_address=$(ip addr show dev eth0 | sed -nr 's/.*inet ([^ ]+)\/.*/\1/p')
+
+#Modify hostname
+detectHostname
+if [[ x$hostname != "x" ]]; then
+  echo "Hostname: ${hostname}"
+  configureScript $admiral_start_script "hostname" ${hostname}
+else
+  echo "Hostname is null, set it to IP"
+  hostname=${ip_address}
+fi
+
+# put admiral endpoint in guestinfo
+$script_dir/set_guestinfo.sh admiral.endpoint https://"$ip_address":"$ADMIRAL_PORT"
+
+# Init certs
+secure
+
+configureScript $admiral_start_script ADMIRAL_DATA_LOCATION $data_dir
+configureScript $admiral_start_script ADMIRAL_EXPOSED_PORT "$ADMIRAL_PORT"
+configureScript $admiral_start_script OVA_VM_IP "$ip_address"
+
+configureScript $admiral_add_default_users_script ADMIRAL_DATA_LOCATION $data_dir
+configureScript $admiral_add_default_users_script ADMIRAL_EXPOSED_PORT "$ADMIRAL_PORT"
+configureScript $admiral_add_default_users_script OVA_VM_IP "$ip_address"
+
+iptables -w -A INPUT -j ACCEPT -p tcp --dport "$ADMIRAL_PORT"
+
+touch $data_dir/custom.conf
+
+
+HARBOR_PORT=$(ovfenv -k registry.port)
+# Configure the integration URL
+echo "harbor.tab.url=https://${hostname}:${HARBOR_PORT}" > $data_dir/custom.conf
+
+# Copy files needed by Admiral into one directory
+config_dir=$data_dir/configs
+mkdir -p $config_dir
+admiral_psc_dir=/etc/vmware/psc/admiral
+
+cp $jks $config_dir
+cp $key $config_dir
+cp $cert $config_dir
+cp $data_dir/custom.conf $config_dir/config.properties
+cp $admiral_psc_dir/psc-config.keystore $config_dir
+cp $admiral_psc_dir/psc-config.properties $config_dir
+
+# Change Admiral's keystore.file value to point to mounted path in container
+sed -i "/\b\(keystore.file\)\b/d" $config_dir/psc-config.properties
+echo "keystore.file=/configs/psc-config.keystore" >> $config_dir/psc-config.properties
+
+# Start on startup
+echo "Enable admiral startup."
+systemctl enable admiral_startup.service
+echo "Enable admiral."
+systemctl enable admiral.service
+echo "Services enabled. exiting..."
