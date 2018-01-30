06afadf62bf6e5f1988e3c49d86f4e63872b32cc
Jason Morris <morrisjason@vmware.com>
2018-01-30 10:36:20 -0600

    Use first grub entry as default

--- installer/build/baseimage/stage.sh
+++ installer/build/baseimage/stage.sh
@@ -210,7 +210,7 @@ function setup_grub() {
   cat > "${root}/boot/grub2/grub.cfg" << EOF
 # Begin /boot/grub2/grub.cfg
 
-set default=5
+set default=0
 set timeout=5
 search -n -u $BOOT_UUID -s
 loadfont ${BOOT_DIRECTORY}grub2/ascii.pf2
