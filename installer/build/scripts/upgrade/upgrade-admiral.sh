98fef8a6a7e1dafcbd0a6f5288c3c8d5ba04c7fc
Jason Morris <morrisjason@vmware.com>
2018-01-30 12:27:04 -0600

    PR Fixes

--- installer/build/scripts/upgrade/upgrade-admiral.sh
+++ installer/build/scripts/upgrade/upgrade-admiral.sh
@@ -64,14 +64,14 @@ function upgradeAdmiral {
 
   tab_retries=0
   max_tab_retries=6 # 60 seconds
-  while [ "$(setTabUrl)" != "200" && ${tab_retries} -lt ${max_tab_retries} ]; do
+  while [[ "$(setTabUrl)" != "200" && ${tab_retries} -lt ${max_tab_retries} ]]; do
     timecho "Waiting for admiral api tab update..."
     sleep 10
     ((tab_retries++))
   done
 
   if [ ${tab_retries} -eq ${max_tab_retries} ]; then
-    timecho "Admiral api could not be reached. Exiting..."
+    timecho "Admiral api could not be reached. Exiting..." | tee /dev/fd/3
     exit 1
   fi
 
