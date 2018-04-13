696f822b28874e9ba0fc8b195887f3ceb1f6d305
Andrew Chin <andrewchin@vmware.com>
2018-04-13 08:40:50 -0700

    update

--- installer/build/scripts/systemd/scripts/vic-appliance-tls.sh
+++ installer/build/scripts/systemd/scripts/vic-appliance-tls.sh
@@ -192,11 +192,6 @@ function checkCertExpiration() {
 mkdir -p ${cert_dir}
 chown -R "${APPLIANCE_SERVICE_UID}":"${APPLIANCE_SERVICE_UID}" ${cert_dir}
 
-# Provided TLS values from environment file
-tls_cert="${TLS_CERT}"
-tls_private_key="${TLS_PRIVATE_KEY}"
-tls_ca_cert="${TLS_CA_CERT}"
-
 # Init certs
 secure
 checkCertExpiration
