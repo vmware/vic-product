164aa62bb0acfe1abe0eb189d2f0a06ba6e9f281
Jason Morris <10388115+morris-jason@users.noreply.github.com>
2018-05-18 14:32:12 -0500

    Update vendored vmware/vic to v1.4.0 (#1757)
    Updates the vendored vmware/vic repo to version
    v1.4.0. Also adjusts dep configuration to prune unused
    packages. Progress to #1433.

--- installer/vendor/github.com/vmware/vic/pkg/vsphere/compute/rp.go
+++ installer/vendor/github.com/vmware/vic/pkg/vsphere/compute/rp.go
@@ -15,8 +15,6 @@
 package compute
 
 import (
-	log "github.com/Sirupsen/logrus"
-
 	"context"
 
 	"github.com/vmware/govmomi/object"
@@ -24,6 +22,7 @@ import (
 	"github.com/vmware/govmomi/vim25/types"
 
 	"github.com/vmware/vic/pkg/errors"
+	"github.com/vmware/vic/pkg/trace"
 	"github.com/vmware/vic/pkg/vsphere/session"
 	"github.com/vmware/vic/pkg/vsphere/vm"
 )
@@ -47,26 +46,37 @@ func NewResourcePool(ctx context.Context, session *session.Session, moref types.
 	}
 }
 
-func (rp *ResourcePool) GetChildrenVMs(ctx context.Context, s *session.Session) ([]*vm.VirtualMachine, error) {
-	var err error
+// VM returns a slice of MoRefs that are the virtual machines of the provided resource pool
+func VM(op trace.Operation, session *session.Session, pool *object.ResourcePool) ([]types.ManagedObjectReference, error) {
 	var mrp mo.ResourcePool
-	var vms []*vm.VirtualMachine
-
-	if err = rp.Properties(ctx, rp.Reference(), []string{"vm"}, &mrp); err != nil {
-		log.Errorf("Unable to get children vm of resource pool %s: %s", rp.Name(), err)
-		return vms, err
+	err := session.Retrieve(op, []types.ManagedObjectReference{pool.Reference()}, []string{"vm"}, &mrp)
+	if err != nil {
+		op.Errorf("Error retrieving VMs for resource pool %s: %s", pool.Name(), err)
+		return nil, err
 	}
+	return mrp.Vm, nil
+}
 
-	for _, o := range mrp.Vm {
-		v := vm.NewVirtualMachine(ctx, s, o)
+// GetChildrenVMs returns a slice of VirtualMachines that are the pools VMs
+func (rp *ResourcePool) GetChildrenVMs(op trace.Operation) ([]*vm.VirtualMachine, error) {
+	var vms []*vm.VirtualMachine
+	refs, err := VM(op, rp.Session, rp.ResourcePool)
+	if err != nil {
+		return nil, err
+	}
+	for _, o := range refs {
+		v := vm.NewVirtualMachine(op, rp.Session, o)
 		vms = append(vms, v)
 	}
 	return vms, nil
 }
 
-func (rp *ResourcePool) GetChildVM(ctx context.Context, s *session.Session, name string) (*vm.VirtualMachine, error) {
-	searchIndex := object.NewSearchIndex(s.Client.Client)
-	child, err := searchIndex.FindChild(ctx, rp.Reference(), name)
+// GetChildVM searches the pool for a VM by name and returns a VirtualMachine
+func (rp *ResourcePool) GetChildVM(ctx context.Context, name string) (*vm.VirtualMachine, error) {
+	op := trace.FromContext(ctx, name)
+
+	searchIndex := object.NewSearchIndex(rp.Vim25())
+	child, err := searchIndex.FindChild(op, rp.Reference(), name)
 	if err != nil {
 		return nil, errors.Errorf("Unable to find VM(%s): %s", name, err.Error())
 	}
@@ -74,37 +84,41 @@ func (rp *ResourcePool) GetChildVM(ctx context.Context, s *session.Session, name
 		return nil, nil
 	}
 	// instantiate the vm object
-	return vm.NewVirtualMachine(ctx, s, child.Reference()), nil
+	return vm.NewVirtualMachine(op, rp.Session, child.Reference()), nil
 }
 
 func (rp *ResourcePool) GetCluster(ctx context.Context) (*object.ComputeResource, error) {
+	op := trace.FromContext(ctx, rp.Name())
+
 	var err error
 	var mrp mo.ResourcePool
 
-	if err = rp.Properties(ctx, rp.Reference(), []string{"owner"}, &mrp); err != nil {
-		log.Errorf("Unable to get cluster of resource pool %s: %s", rp.Name(), err)
+	if err = rp.Properties(op, rp.Reference(), []string{"owner"}, &mrp); err != nil {
+		op.Errorf("Unable to get cluster of resource pool %s: %s", rp.Name(), err)
 		return nil, err
 	}
 
-	return object.NewComputeResource(rp.Client.Client, mrp.Owner), nil
+	return object.NewComputeResource(rp.Vim25(), mrp.Owner), nil
 }
 
 func (rp *ResourcePool) GetDatacenter(ctx context.Context) (*object.Datacenter, error) {
-	dcRef, err := rp.getLowestAncestor(ctx, "Datacenter")
+	op := trace.FromContext(ctx, rp.Name())
+
+	dcRef, err := rp.getLowestAncestor(op, "Datacenter")
 	if err != nil || dcRef == nil {
-		log.Errorf("Unable to get datacenter ancestor of rp %s: %s", rp.Name(), err)
+		op.Errorf("Unable to get datacenter ancestor of rp %s: %s", rp.Name(), err)
 		return nil, errors.Errorf("Unable to get datacenter ancestor of rp %s: %s", rp.Name(), err)
 	}
 
-	return object.NewDatacenter(rp.Client.Client, *dcRef), nil
+	return object.NewDatacenter(rp.Vim25(), *dcRef), nil
 }
 
-func (rp *ResourcePool) getAncestors(ctx context.Context, inType string) ([]types.ManagedObjectReference, error) {
+func (rp *ResourcePool) getAncestors(op trace.Operation, inType string) ([]types.ManagedObjectReference, error) {
 	client := rp.Session.Vim25()
 
-	ancestors, err := mo.Ancestors(ctx, client, client.ServiceContent.PropertyCollector, rp.Reference())
+	ancestors, err := mo.Ancestors(op, client, client.ServiceContent.PropertyCollector, rp.Reference())
 	if err != nil {
-		log.Errorf("Unable to get ancestors of rp %s: %s", rp.Name(), err)
+		op.Errorf("Unable to get ancestors of rp %s: %s", rp.Name(), err)
 		return nil, err
 	}
 
@@ -119,10 +133,10 @@ func (rp *ResourcePool) getAncestors(ctx context.Context, inType string) ([]type
 	return outAncestors, nil
 }
 
-func (rp *ResourcePool) getLowestAncestor(ctx context.Context, inType string) (*types.ManagedObjectReference, error) {
-	ancestors, err := rp.getAncestors(ctx, inType)
+func (rp *ResourcePool) getLowestAncestor(op trace.Operation, inType string) (*types.ManagedObjectReference, error) {
+	ancestors, err := rp.getAncestors(op, inType)
 	if err != nil {
-		log.Errorf("Unable to get ancestors of rp %s: %s", rp.Name(), err)
+		op.Errorf("Unable to get ancestors of rp %s: %s", rp.Name(), err)
 		return nil, err
 	}
 
