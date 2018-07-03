vmware/vic-product

commit 3c5cd51aab5a38df37b6bcb6ad28d70d34bad558

Use environment file for most values from ovfenv (#1545)

installer/build/scripts/systemd/scripts/vic-appliance-environment.sh
@@ -18,21 +18,52 @@ umask 077
 
 ENV_FILE="/etc/vmware/environment"
 
-TLS_CERT="$(ovfenv -k appliance.tls_cert)"
-TLS_PRIVATE_KEY="$(ovfenv -k appliance.tls_cert_key)"
-TLS_CA_CERT="$(ovfenv -k appliance.ca_cert)"
+# Keep \n characters in PEM encoded values
+APPLIANCE_TLS_CERT="$(ovfenv -k appliance.tls_cert | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')"
+APPLIANCE_TLS_PRIVATE_KEY="$(ovfenv -k appliance.tls_cert_key | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')"
+APPLIANCE_TLS_CA_CERT="$(ovfenv -k appliance.ca_cert | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')"
+
 ADMIRAL_PORT="$(ovfenv -k management_portal.port)"
 REGISTRY_PORT="$(ovfenv -k registry.port)"
 NOTARY_PORT="$(ovfenv -k registry.notary_port)"
 FILESERVER_PORT="$(ovfenv -k appliance.config_port)"
 HOSTNAME=""
 IP_ADDRESS=""
+DEFAULT_USERS_CREATE_DEF_USERS="$(ovfenv -k default_users.create_def_users)"
+DEFAULT_USERS_DEF_USER_PREFIX="$(ovfenv -k default_users.def_user_prefix)"
+DEFAULT_USERS_DEF_USER_PASSWORD="$(ovfenv -k default_users.def_user_password)"
+REGISTRY_GC_ENABLED="$(ovfenv --key registry.gc_enabled)"
 
-function detectHostname {
+# TODO split into separate unit to run before ovf-network and network-online
+APPLIANCE_PERMIT_ROOT_LOGIN="$(ovfenv --key appliance.permit_root_login)"
+NETWORK_FQDN="$(ovfenv --key network.fqdn)"
+NETWORK_IP0="$(ovfenv --key network.ip0)"
+NETWORK_NETMASK0="$(ovfenv --key network.netmask0)"
+NETWORK_GATEWAY="$(ovfenv --key network.gateway)"
+NETWORK_DNS="$(ovfenv --key network.DNS | sed 's/,/ /g' | tr -s ' ')"
+NETWORK_SEARCHPATH="$(ovfenv --key network.searchpath)"
+
+function detectHostname() {
   HOSTNAME=$(hostnamectl status --static) || true
   if [ -n "$HOSTNAME" ]; then
     echo "Using hostname from 'hostnamectl status --static': $HOSTNAME"
     return
+  fi
+}
+
+function firstboot() {
+  set +e
+  echo "root:$(ovfenv --key appliance.root_pwd)" | chpasswd
+  # Reset password expiration to 90 days by default
+  chage -d $(date +"%Y-%m-%d") -m 0 -M 90 root
+  set -e
+}
+
+function clearPrivate() {
+  # We then obscure the root password, if the VM is reconfigured with another
+  # password after deployment, we don't act on it and keep obscuring it.
+  if [[ $(ovfenv --key appliance.root_pwd) != '*******' ]]; then
+    ovfenv --key appliance.root_pwd --set '*******'
   fi
 }
 
@@ -59,15 +90,33 @@ echo "Using hostname: ${HOSTNAME}"
 
 
 {
-  echo "TLS_CERT=${TLS_CERT}";
-  echo "TLS_PRIVATE_KEY=${TLS_PRIVATE_KEY}";
-  echo "TLS_CA_CERT=${TLS_CA_CERT}";
-  echo "ADMIRAL_PORT=${ADMIRAL_PORT}";
-  echo "REGISTRY_PORT=${REGISTRY_PORT}";
-  echo "NOTARY_PORT=${NOTARY_PORT}";
-  echo "FILESERVER_PORT=${FILESERVER_PORT}";
   echo "VIC_MACHINE_SERVER_PORT=8443";
   echo "APPLIANCE_SERVICE_UID=10000";
   echo "HOSTNAME=${HOSTNAME}";
   echo "IP_ADDRESS=${IP_ADDRESS}";
+  echo "ADMIRAL_PORT=${ADMIRAL_PORT}";
+  echo "REGISTRY_PORT=${REGISTRY_PORT}";
+  echo "NOTARY_PORT=${NOTARY_PORT}";
+  echo "FILESERVER_PORT=${FILESERVER_PORT}";
+  echo "APPLIANCE_TLS_CERT=${APPLIANCE_TLS_CERT}";
+  echo "APPLIANCE_TLS_PRIVATE_KEY=${APPLIANCE_TLS_PRIVATE_KEY}";
+  echo "APPLIANCE_TLS_CA_CERT=${APPLIANCE_TLS_CA_CERT}";
+  echo "DEFAULT_USERS_CREATE_DEF_USERS=${DEFAULT_USERS_CREATE_DEF_USERS}";
+  echo "DEFAULT_USERS_DEF_USER_PREFIX=${DEFAULT_USERS_DEF_USER_PREFIX}";
+  echo "DEFAULT_USERS_DEF_USER_PASSWORD=${DEFAULT_USERS_DEF_USER_PASSWORD}";
+  echo "REGISTRY_GC_ENABLED=${REGISTRY_GC_ENABLED}";
+  echo "APPLIANCE_PERMIT_ROOT_LOGIN=${APPLIANCE_PERMIT_ROOT_LOGIN}";
+  echo "NETWORK_FQDN=${NETWORK_FQDN}";
+  echo "NETWORK_IP0=${NETWORK_IP0}";
+  echo "NETWORK_NETMASK0=${NETWORK_NETMASK0}";
+  echo "NETWORK_GATEWAY=${NETWORK_GATEWAY}";
+  echo "NETWORK_DNS=${NETWORK_DNS}";
+  echo "NETWORK_SEARCHPATH=${NETWORK_SEARCHPATH}";
 } > ${ENV_FILE}
+
+# Only run on first boot
+if [[ ! -f /etc/vmware/firstboot ]]; then
+  firstboot
+fi
+# Remove private values from ovfenv
+clearPrivate

installer/build/scripts/admiral/add_default_users.sh
@@ -28,13 +28,14 @@ function get_property
     grep "^$2=" "$1" | cut -d'=' -f2
 }
 
-create_def_users=$(ovfenv -k default_users.create_def_users)
-user_prefix=$(ovfenv -k default_users.def_user_prefix)
-user_password=$(ovfenv -k default_users.def_user_password)
+# Values from vic-appliance-environment
+create_def_users=${DEFAULT_USERS_CREATE_DEF_USERS}
+user_prefix=${DEFAULT_USERS_DEF_USER_PREFIX}
+user_password=${DEFAULT_USERS_DEF_USER_PASSWORD}
 
 echo "add_default_users: $create_def_users, $user_prefix"
 
-if [ ${create_def_users} != "True" ] || [ -z ${user_prefix} ] || [ -z ${user_password} ]; then
+if [ "${create_def_users}" != "True" ] || [ -z "${user_prefix}" ] || [ -z "${user_password}" ]; then
     echo "add_default_users, not creating default users"
     exit 0
 fi
@@ -56,15 +57,19 @@ while true ; do
     fi
 done
 
-token=`cat $token_file`
+token=$(cat "$token_file")
 
 echo "add_default_users loaded token"
 
-tenant=`get_property $psc_prop_file "tenant"`
-defuser_prefix=`get_property $psc_prop_file "default-user-prefix"`
-admiral_url=`get_property $psc_prop_file admiral-url`
+tenant=$(get_property $psc_prop_file "tenant")
+defuser_prefix=$(get_property $psc_prop_file "default-user-prefix")
+admiral_url=$(get_property $psc_prop_file admiral-url)
 # remove backslashes
-admiral_url=`echo $admiral_url | sed 's/\\\//g'`
+admiral_url=`echo "$admiral_url" | sed 's/\\\//g'`
+
+echo "tenant: $tenant"
+echo "defuser_prefix: $defuser_prefix"
+echo "admiral_url: $admiral_url"
 
 cloud_admin_name=$defuser_prefix
 cloud_admin_name+="-cloud-admin"
@@ -88,7 +93,7 @@ while true ; do
 
     echo "add_default_users ping result: ${http_code}"
 
-    if [ ${http_code} -eq "200" ]; then
+    if [ "${http_code}" -eq "200" ]; then
         break;
     fi
 

installer/build/scripts/harbor/start_harbor.sh
@@ -41,8 +41,8 @@ function gc {
   echo "===================================================="
 }
 
-gc_enabled=$(ovfenv --key registry.gc_enabled)
-if [ "${gc_enabled,,}" == "true" ]; then
+# From vic-appliance-environment
+if [ "${REGISTRY_GC_ENABLED}" == "true" ]; then
   gc 2>&1 >> /var/log/harbor/gc.log || true
 fi
 

installer/build/scripts/systemd/scripts/load-docker-images.sh
@@ -16,12 +16,7 @@ set -euf -o pipefail
 source /installer.env
 
 if [[ ! -f /etc/vmware/firstboot ]]; then
-  # change root password
   set +e
-  echo "root:$(ovfenv --key appliance.root_pwd)" | chpasswd
-  # Reset password expiration to 90 days by default
-  chage -d $(date +"%Y-%m-%d") -m 0 -M 90 root
-
   # Only load the docker images if it's the first time booting
   ls "/etc/cache/docker/" | while read line; do
     docker load -i "/etc/cache/docker/$line"
@@ -60,10 +55,4 @@ if [[ ! -f /etc/vmware/firstboot ]]; then
   date -u +"%Y-%m-%dT%H:%M:%SZ" > /etc/vmware/firstboot
 else
   echo "No images to load...."
-fi
-
-# We then obscure the root password, if the VM is reconfigured with another
-# password after deployment, we don't act on it and keep obscuring it.
-if [[ $(ovfenv --key appliance.root_pwd) != '*******' ]]; then
-  ovfenv --key appliance.root_pwd --set '*******'
 fi

installer/build/scripts/systemd/scripts/network-config.sh
@@ -24,16 +24,17 @@ mask2cdr () {
 netConfig=''
 dhcpOpts=''
 
-fqdn=$(ovfenv --key network.fqdn)
-network_address=$(ovfenv --key network.ip0)
-netmask=$(ovfenv --key network.netmask0)
-gateway=$(ovfenv --key network.gateway)
-dns=$(ovfenv --key network.DNS | sed 's/,/ /g' | tr -s ' ')
-domains=$(ovfenv --key network.searchpath)
+# TODO use environment file
+fqdn="$(ovfenv --key network.fqdn)"
+network_address="$(ovfenv --key network.ip0)"
+netmask="$(ovfenv --key network.netmask0)"
+gateway="$(ovfenv --key network.gateway)"
+dns="$(ovfenv --key network.DNS | sed 's/,/ /g' | tr -s ' ')"
+domains="$(ovfenv --key network.searchpath)"
 
 # static OR DHCP options.
 if [[ -n $network_address || -n $netmask || -n $gateway ]]; then
-  netConfig="Address=${network_address}/$(mask2cdr $netmask)\n\
+  netConfig="Address=${network_address}/$(mask2cdr "$netmask")\n\
          Gateway=$gateway\n"
 else
   netConfig="DHCP=ipv4\n"
@@ -53,7 +54,7 @@ fi
 
 # always set the fqdn if it exists using hostnamectl
 if [[ -n $fqdn ]]; then
-  hostnamectl set-hostname $fqdn
+  hostnamectl set-hostname "$fqdn"
   dhcpOpts+="UseHostname=false\n"
 fi
 
@@ -62,7 +63,7 @@ cat <<EOF | tee ${network_conf_file}
 Name=eth0
 
 [Network]
-$(echo -e $netConfig)
+$(echo -e "$netConfig")
 
 [DHCP]
 $(echo -e $dhcpOpts)

installer/build/scripts/systemd/scripts/sshd_permitrootlogin.sh
@@ -16,11 +16,11 @@
 # remove all default settings
 sed -i "/^PermitRootLogin.*/d" /etc/ssh/sshd_config
 
-# fetch user setting
-PERMIT=$(ovfenv --key appliance.permit_root_login)
+# TODO use environment file
+PERMIT="$(ovfenv --key appliance.permit_root_login)"
 
-# currently only accepts True as yes
-if [ ${PERMIT,,} == "true" ]; then
+# Only accepts True as yes
+if [ "${PERMIT,,}" == "true" ]; then
   echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 else
   echo "PermitRootLogin no" >> /etc/ssh/sshd_config

installer/build/scripts/systemd/scripts/support/appliance-support.sh
@@ -107,6 +107,7 @@ function getPrivateFiles {
   echo "Including private values in log bundle"
 
   commandToFile "ovfenv" "ovfenv" "appliance"
+  commandToFile "cat /etc/vmware/environment" "environment" "appliance"
   commandToFile "openssl x509 -in /storage/data/certs/ca.crt -text -noout" "ca.crt" "certs"
   commandToFile "openssl x509 -in /storage/data/certs/server.crt -text -noout" "server.crt" "certs"
   commandToFile "cat /storage/data/certs/cert_gen_type" "cert_gen_type" "certs"
@@ -124,14 +125,20 @@ function getPrivateFiles {
   set -e
 }
 
+# filterEnvironment filters private values from the environment file
+function filterEnvironment {
+  commandToFile "cat /etc/vmware/environment | grep -v APPLIANCE_TLS_PRIVATE_KEY" "environment_filtered" "appliance"
+}
+
 # getDiagInfo gathers diagnostic info and logs
 function getDiagInfo {
   # Appliance
+  filterEnvironment
   commandToFile "hostnamectl" "hostnamectl" "appliance"
   commandToFile "timedatectl" "timedatectl" "appliance"
   commandToFile "ip address show" "ip_addr" "appliance"
-  commandToFile "cat /etc/vmware/environment" "environment" "appliance"
   commandToFile "cat /etc/vmware/firstboot" "firstboot" "appliance"
+  commandToFile "cat /registration-timestamps.txt" "registration-timestamps.txt" "appliance"
   commandToFile "uptime" "uptime" "appliance"
   commandToFile "cat /etc/vmware/version" "appliance_version" "appliance"
   commandToFile "cat /storage/data/version" "data_version" "appliance"

installer/build/scripts/systemd/scripts/vic-appliance-tls.sh
@@ -31,6 +31,11 @@ key="${cert_dir}/server.key"
 flag="${cert_dir}/cert_gen_type"
 jks="${cert_dir}/trustedcertificates.jks"
 
+# From vic-appliance-environment
+tls_cert="${APPLIANCE_TLS_CERT}"
+tls_private_key="${APPLIANCE_TLS_PRIVATE_KEY}"
+tls_ca_cert="${APPLIANCE_TLS_CA_CERT}"
+
 # Format cert file
 function formatCert {
   content=$1
@@ -89,18 +94,17 @@ function genCert {
 }
 
 function secure {
-  if [ -n "${TLS_PRIVATE_KEY}" ] && [ "${TLS_PRIVATE_KEY}" == "*ENCRYPTED*" ]; then
+  if [ -n "$tls_private_key" ] && [ "$tls_private_key" == "*ENCRYPTED*" ]; then
     echo "Private key is encrypted - will generate a self-signed certificate"
     genCert
     return
   fi
 
-  # Provided TLS values from environment file
-  if [ -n "${TLS_CERT}" ] && [ -n "${TLS_PRIVATE_KEY}" ] && [ -n "${TLS_CA_CERT}" ]; then
+  if [ -n "$tls_cert" ] && [ -n "$tls_private_key" ] && [ -n "$tls_ca_cert" ]; then
     echo "TLS certificate, private key, and CA certificate are set, using customized certificate"
-    formatCert "${TLS_CERT}" $cert
-    formatCert "${TLS_PRIVATE_KEY}" $key
-    formatCert "${TLS_CA_CERT}" $ca_cert
+    formatCert "$tls_cert" $cert
+    formatCert "$tls_private_key" $key
+    formatCert "$tls_ca_cert" $ca_cert
 
     echo "customized" > $flag
 

commit 6f82a5d0bf0c281e0e44c422fb8be55cc2c10163

Prevent password masking from becoming password
There is a path, likely retry or recoverying but at this time unconfirmed,
by which the password masking can be set as the password. This expressly
checks for the value having been masked and refuses to change the password
to that value.

This is a very focused change to address specifically the observed
behaviour as the implications of a revert or moving the code to other
units are not understood at this time.
Issue was introduced in 3c5cd51a

installer/build/scripts/systemd/scripts/vic-appliance-environment.sh
@@ -14,6 +14,8 @@
 # limitations under the License.
 set -euf -o pipefail
 
+declare -r mask="*******"
+
 umask 077
 
 ENV_FILE="/etc/vmware/environment"
@@ -53,7 +55,13 @@ function detectHostname() {
 
 function firstboot() {
   set +e
-  echo "root:$(ovfenv --key appliance.root_pwd)" | chpasswd
+  local tmp
+  tmp="$(ovfenv --key appliance.root_pwd)"
+  if [[ "$tmp" == "$mask" ]]; then
+    return
+  fi
+
+  echo "root:$tmp" | chpasswd
   # Reset password expiration to 90 days by default
   chage -d $(date +"%Y-%m-%d") -m 0 -M 90 root
   set -e
@@ -62,8 +70,8 @@ function firstboot() {
 function clearPrivate() {
   # We then obscure the root password, if the VM is reconfigured with another
   # password after deployment, we don't act on it and keep obscuring it.
-  if [[ $(ovfenv --key appliance.root_pwd) != '*******' ]]; then
-    ovfenv --key appliance.root_pwd --set '*******'
+  if [[ "$(ovfenv --key appliance.root_pwd)" != "$mask" ]]; then
+    ovfenv --key appliance.root_pwd --set "$mask"
   fi
 }
 