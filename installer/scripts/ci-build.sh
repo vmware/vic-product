b44cd361a76c48cad43ae49da75180f7c2f54aa3
Zach Shepherd <zjs@users.noreply.github.com>
2018-08-23 19:22:19 +0800

    Integrate pre-tag components in release builds (#1961)
    Consuming only tagged components in non-dev tag builds ensures that
    our release candidates and releases include only the expected, stable,
    version of each component.
    
    Consuming those same tagged components in push and pull_request builds
    is beneficial because it maintains consistency between builds leading
    up to a tag build and the tag build itself.
    
    However, this has a significant disadvantage: it means that components
    being developed on release branches concurrently are not continuously
    integrated; the first time integration occurs is when RC1 is tagged.
    
    To allow testing of OVA builds sooner, we can consume new versions of
    components more aggressively for pull_request builds, push builds, and
    dev tag builds.
    
    For Admiral, we will consume the same vic_dev tag consumed on master.
    
    For the VIC engine .tar.gz and vic-machine-server container image, we
    consume the latest build from the corresponding release branch. Note
    that we assume that the version number of the OVA is the same as the
    version number of VIC. To produce a release where this assumption is
    not not valid (e.g., a patch release of VIC product where the version
    of VIC itself is not changed), the script could be modified directly
    on the release branch to hard-code the appropriate VIC version.
    
    (cherry picked from commit 9af80bfcbe2c9db866afb332f3406490c9b2df90)

--- installer/scripts/ci-build.sh
+++ installer/scripts/ci-build.sh
@@ -39,16 +39,23 @@ fi
 # set release options if drone args not present
 if [[ ( "$DRONE_BUILD_EVENT" == "tag" && "$DRONE_TAG" != *"dev"* ) || "$DRONE_BRANCH" == *"releases/"* ]]; then
   if [ -z "${ADMIRAL}" ]; then
-    admiral_release=$(curl -s https://hub.docker.com/v2/repositories/vmware/admiral/tags/\?page\=1\&page_size\=250 | jq '.results[] | .name'| cut -d "\"" -f2 | grep '^vic_v' | head -n 1 | cut -d'_' -f2)
-    OPTIONS="--admiral $admiral_release"
+    if [[ "$DRONE_BUILD_EVENT" == "tag" && "$DRONE_TAG" != *"dev"* ]]; then
+      admiral_release=$(curl -s https://hub.docker.com/v2/repositories/vmware/admiral/tags/\?page\=1\&page_size\=250 | jq '.results[] | .name'| cut -d "\"" -f2 | grep '^vic_v' | head -n 1 | cut -d'_' -f2)
+      OPTIONS="--admiral $admiral_release"
+    fi
   fi
   if [ -z "${HARBOR}" ]; then
     harbor_release=$(gsutil ls -l "gs://harbor-releases" | grep -v TOTAL | grep offline-installer | sort -k2 -r | (trap '' PIPE; head -n1) | xargs | cut -d ' ' -f 3 | sed 's/gs:\/\//https:\/\/storage.googleapis.com\//')
     OPTIONS="$OPTIONS --harbor $harbor_release"
   fi
   if [ -z "${VICENGINE}" ]; then
-    vicengine_release=$(gsutil ls -l "gs://vic-engine-releases" | grep -v TOTAL | grep vic_ | sort -k2 -r | (trap '' PIPE; head -1) | xargs | cut -d ' ' -f 3 | sed 's/gs:\/\//https:\/\/storage.googleapis.com\//')
-    OPTIONS="$OPTIONS --vicengine $vicengine_release"
+    if [[ "$DRONE_BUILD_EVENT" != "tag" || "$DRONE_TAG" == *"dev"* ]]; then
+      bucket="gs://vic-engine-builds/$DRONE_BRANCH"
+    else
+      bucket="gs://vic-engine-releases"
+    fi
+    vicengine_release=$(gsutil ls -l "$bucket" | grep -v TOTAL | grep vic_ | sort -k2 -r | (trap '' PIPE; head -1) | xargs | cut -d ' ' -f 3 | sed 's/gs:\/\//https:\/\/storage.googleapis.com\//')
+    OPTIONS="$OPTIONS --vicengine ${vicengine_release:?Unable to find an appropriate VIC Engine build. Is '"'$DRONE_BRANCH'"' a valid vmware/vic branch?}"
   fi
   if [ -z "${VIC_MACHINE_SERVER}" ]; then
     # Listing container tags requires permissions
@@ -64,8 +71,14 @@ if [[ ( "$DRONE_BUILD_EVENT" == "tag" && "$DRONE_TAG" != *"dev"* ) || "$DRONE_BR
       rm -f ${KEY_FILE}
     fi
 
-    vicmachineserver_release="$(gcloud container images list-tags gcr.io/eminent-nation-87317/vic-machine-server --filter='tags~.' | grep -v DIGEST | awk '{print $2}' | sed -rn 's/^(.*,)?(v([0-9]+\.){2}[0-9]+(-rc[0-9]+)?)(,.*)?$/\2/p' | head -n 1)"
-    OPTIONS="$OPTIONS --vicmachineserver $vicmachineserver_release"
+    if [[ "$DRONE_BUILD_EVENT" != "tag" || "$DRONE_TAG" == *"dev"* ]]; then
+      version="v${DRONE_BRANCH#releases/}-.*"
+    else
+      version="v([0-9]+\.){2}[0-9]+(-rc[0-9]+)?"
+    fi
+
+    vicmachineserver_release="$(gcloud container images list-tags gcr.io/eminent-nation-87317/vic-machine-server --filter='tags~.' | grep -v DIGEST | awk '{print $2}' | sed -rn 's/^(.*,)?('"${version}"')(,.*)?$/\2/p' | head -n 1)"
+    OPTIONS="$OPTIONS --vicmachineserver ${vicmachineserver_release:?Unable to find an appropriate vic-machine-server image. Is '"'$DRONE_BRANCH'"' a valid vmware/vic branch?}"
   fi
 fi
 
